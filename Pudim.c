c1ff58e347e083789ddf82cda2703398b530becc
Luiz Frederico <luluiz21@hotmail.com>
2016-03-01 22:37:51 -0300

    commit inicial

---
+++ Pudim.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(int argc, char *argv[])
+{
+  printf("Ola senhorita\n" "oi senhor\n"
+  	"Vamos comer um pudim?\n"
+  	"claro, vamos sim !\n");
+
+  exit(0);
+}
